6dce365dff635a31554bbc3526f41f6cb9f9a991
Ellen Chen <echen078@g.ucla.edu>
2026-05-12 00:30:12 -0700

    Add demo pins to Map component for enhanced visualization

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,13 @@
 import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet'
+import MapPin from './MapPin.jsx'
 
 const LOS_ANGELES_COORDS = [34.0522, -118.2437]
+const DEMO_PINS = [
+  { label: 'Santa Monica Pier', position: [34.0094, -118.4973] },
+  { label: 'Griffith Observatory', position: [34.1184, -118.3004] },
+  { label: 'UCLA', position: [34.0689, -118.4452] },
+  { label: 'Grand Central Market', position: [34.0508, -118.2494] },
+]
 
 function MapClickLogger() {
   useMapEvents({
@@ -24,6 +31,11 @@ function Map() {
         attribution='&copy; OpenStreetMap contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
+      {DEMO_PINS.map((pin) => (
+        <MapPin key={pin.label} position={pin.position}>
+          {pin.label}
+        </MapPin>
+      ))}
       <MapClickLogger />
     </MapContainer>
   )
