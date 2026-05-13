a30955d3536adbb3732dcaf9e58b3cef4918340a
gn-67 <gokul.nambiar91@gmail.com>
2026-05-13 00:29:15 -0700

    configure multer for lcal file storage

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
@@ -11,6 +12,7 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 app.get('/api/health', (req, res) => {
