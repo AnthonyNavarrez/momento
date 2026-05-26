37e978a83f1adf23351f6bb6d94de124ea522362
Ellen Chen <echen078@g.ucla.edu>
2026-05-25 18:24:12 -0700

    added optionalAuth for handling optional token authentication.

--- server/routes/photoRoutes.js
+++ server/routes/photoRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { uploadPhoto, getUserPhotos, getPhotoById, deletePhoto, getHeatmapData } = require('../controllers/photoController');
-const protect = require('../middleware/auth');
+const { protect } = require('../middleware/auth');
 const upload = require('../config/upload');
 
 router.post('/', protect, upload.single('photo'), uploadPhoto);
