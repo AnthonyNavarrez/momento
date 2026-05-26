93c8fa26cec910644744b2278c6b8dd0aad8459c
Ellen Chen <echen078@g.ucla.edu>
2026-05-25 19:19:52 -0700

    add routing and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { SignupPage } from './pages/signup'
 import { MapPage } from './pages/Map'
 import { LoginPage } from './pages/LoginPage'
 import { GalleryPage } from './pages/GalleryPage'
+import { ExplorePage } from './pages/ExplorePage'
 import { AuthProvider } from './context/AuthContext'
 import { ProtectedRoute } from './components/ProtectedRoute'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
@@ -19,6 +20,7 @@ function App() {
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/map" element={<ProtectedRoute><MapPage /></ProtectedRoute>} />
             <Route path="/gallery" element={<ProtectedRoute><GalleryPage /></ProtectedRoute>} />
+            <Route path="/explore" element={<ExplorePage />} />
           </Routes>
         </AuthProvider>
       </BrowserRouter>
